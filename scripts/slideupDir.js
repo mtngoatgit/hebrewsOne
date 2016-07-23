1ebf22fd7043e408e2db633cf9f8b811ebc01fa2
Paxton Bigler <paxton.bigler@gmail.com>
2016-07-23 16:06:22 -0600

    bulk of code finished

--- scripts/slideupDir.js
+++ scripts/slideupDir.js
@@ -18,32 +18,95 @@ angular.module('hebrewApp').directive('slideDir', function(){
                 $(this).css("background-color", "#e85530")
             })
 
-            //  $("#home-butt").hover(function(){
-            //     $(this).css("background-color", "#2a3132");
-            // }, function(){
-            //     $(this).css("background", "none")
-            // })
+             $("#home-butt").hover(function(){
+                $(this).css("color", "#2a3132");
+            }, function(){
+                $(this).css("color", "white")
+            })
 
             $(".comm-togg").hover(function(){
-                $(this).css("background-color", "#e85530")
+                $(this).css("background-color", "#2a3132")
                 .css("cursor", "pointer");
             }, function(){
                 $(this).css("background", "none")
             })
-                $(function() {        
-		    $(".welcome-word").typed({
-			    strings: ["welcome..."],
-			    typeSpeed: 30,
-			    showCursor: false
-		    });
+
+            $(".english-container").hover(function(){
+                $(this).css("background-color", "#2a3132");
+            }, function(){
+                $(this).css("background", "none")
             })
-               $(function() {        
-		    $(".choose-verse").typed({
-			    strings: ["choose verses here"],
-			    typeSpeed: 10,
-			    showCursor: false
-		    });
+
+             $(".greek-container").hover(function(){
+                $(this).css("background-color", "#2a3132");
+            }, function(){
+                $(this).css("background", "none")
             })
+             $(".ling-comm").hover(function(){
+                $(this).css("background-color", "#2a3132");
+                $('#titulary').css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $('#titulary').css("color", "#2a3132");
+            })
+
+              $(".phil-comm").hover(function(){
+                $(this).css("background-color", "#2a3132");
+                $('#titulary').css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $('#titulary').css("color", "#2a3132");
+            })
+
+              $(".climb").hover(function(){
+                $(this).css("background-color", "#2a3132")
+                .css("cursor", "pointer");
+                $(this).css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $(this).css("color", "#2a3132");
+            })
+              $(".run").hover(function(){
+                $(this).css("background-color", "#2a3132")
+                .css("cursor", "pointer");
+                $(this).css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $(this).css("color", "#2a3132");
+            })
+              $(".sail").hover(function(){
+                $(this).css("background-color", "#2a3132")
+                .css("cursor", "pointer");
+                $(this).css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $(this).css("color", "#2a3132");
+            })
+              $(".bravado").hover(function(){
+                $(this).css("background-color", "#2a3132")
+                .css("cursor", "pointer");
+                $(this).css("color", "white");
+            }, function(){
+                $(this).css("background", "none")
+                $(this).css("color", "#2a3132");
+            })
+
+
+
+            //     $(function() {        
+		    // $(".welcome-word").typed({
+			//     strings: ["welcome..."],
+			//     typeSpeed: 30,
+			//     showCursor: false
+		    // });
+            // })
+            //    $(function() {        
+		    // $(".choose-verse").typed({
+			//     strings: ["choose verses here"],
+			//     typeSpeed: 10,
+			//     showCursor: false
+		    // });
+            // })
         
     }//end of link
 }
--- scripts/textHighlightDir.js
+++ scripts/textHighlightDir.js
@@ -3,28 +3,31 @@ angular
     .directive("textHighlightDir", function(){
         return {
             restrict: 'A',
-            controller: function($scope, mainSvc){
-                $scope.getData = mainSvc.getData().then(function(response){
-                    $scope.commentary = response.one.linguistic;
-                })
+            // controller: function($scope, mainSvc){
+            //     $scope.getData = mainSvc.getData().then(function(response){
+            //         $scope.commentary = response.one.linguistic;
+            //     })
+            // },
+            scope: {
+                circumflex: '='
             },
             link: function(scope, element, attrs) {
-                
-                scope.commentary.then(function(response){
                     function splitString(stringToSplit, separator){
-                        var arrayOfStrings = stringToSplit.spli(separator);
+                        var arrayOfStrings = stringToSplit.split(separator);
                         return arrayOfStrings;
                     }
                     var space= " ";
-                    scope.newCommentary = splitString(response, space);
+                    scope.newCommentary = splitString(this, space);
             
                     for (var i = 0; i < scope.newCommentary.length; i++){
                         if(scope.newCommentary.charAt[0] === '^'){
                             attrs.css("background-color", "red");
                         }//end of if statement
                     }//end of for loop
-                })//end of scope.commentary
             }//end of link
         }//end of return
     })//end of directive
 
+
+
+// I WANT TO BE ABLE TO EXPORT FUNCTIONALITY 
